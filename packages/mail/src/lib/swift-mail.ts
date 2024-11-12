c470d3a5337592aa7df9c8e6a8b74dd54f452adc
Jonas Tomanga <celleb@mrcelleb.com>
2024-11-12 23:46:09 +0100

    feat: enhance email types and templates with new data structures ✉️

--- packages/mail/src/lib/swift-mail.ts
+++ packages/mail/src/lib/swift-mail.ts
@@ -9,16 +9,52 @@ type SwiftMailOptions = SMTPTransport.Options & {
   url?: string;
 };
 
-type SwiftConfirmationEmail = {
-  name?: string;
-  company?: string;
-  intro?: string;
-  confirmationLink: string;
+type SwiftMailBaseOptions = {
+  companyName: string;
+  logo?: string;
+  baseUri?: string;
   footer?: string;
+  name?: string;
   note?: string;
+  intro?: string;
 };
 
-type SwiftMailSendOptions = Partial<Parameters<SwiftMail["sendMail"]>[0]>;
+type SwiftConfirmationEmailData = SwiftMailBaseOptions & {
+  link: string;
+};
+
+type SwiftPasswordResetEmailData = SwiftMailBaseOptions & {
+  link: string;
+  footer?: string;
+};
+
+type SwiftPasswordlessLoginEmailData = SwiftMailBaseOptions & {
+  link: string;
+};
+
+type SwiftWelcomeEmailData = SwiftMailBaseOptions & {
+  ctaLabel?: string;
+  message?: string;
+};
+
+type SwiftAcceptInvitationEmailData = SwiftMailBaseOptions & {
+  link: string;
+};
+
+type SwiftPasswordInvitationEmailData = SwiftMailBaseOptions & {
+  link: string;
+};
+
+type SwiftWelcomeWithCredentialsEmailData = SwiftMailBaseOptions & {
+  link: string;
+  credentials: { label: string; value: string }[];
+};
+
+type SwiftMailSendOptions = Partial<
+  Omit<Parameters<SwiftMail["sendMail"]>[0], "html">
+> & {
+  to: string;
+};
 
 export interface SwiftMail<T extends Transport<unknown> = SMTPTransport>
   extends Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options> {}
@@ -55,25 +91,117 @@ export class SwiftMail {
     this.parser = new Templ({ baseDir: this.config.templatesDir });
   }
 
-  async sendWelcomeEmail(
-    options: SwiftMailSendOptions,
-    data: SwiftConfirmationEmail
+  async sendConfirmationEmail(
+    {
+      from = this.config.from as string,
+      subject = "Confirm your email address",
+      ...rest
+    }: SwiftMailSendOptions,
+    data: SwiftConfirmationEmailData
   ) {
     const html = await this.getConfirmationEmailHtml(data);
-    return this.sendMail({ ...options, html });
+    return this.sendMail({ from, ...rest, html });
   }
 
-  async getConfirmationEmailHtml(data: SwiftConfirmationEmail) {
+  async getConfirmationEmailHtml(data: SwiftConfirmationEmailData) {
     return this.parser.render("email-confirmation.html", data);
   }
 
-  async sendPasswordResetEmail(to: string, data: any) {
-    const template = await this.parser.render("password-reset.html", data);
-    await this.sendMail({ to, subject: "Password Reset", html: template });
+  async sendPasswordResetEmail(
+    {
+      from = this.config.from as string,
+      subject = "Password reset",
+      ...rest
+    }: SwiftMailSendOptions,
+    data: SwiftPasswordResetEmailData
+  ) {
+    const html = await this.getPasswordResetEmailHtml(data);
+    return this.sendMail({ from, ...rest, html });
   }
 
-  async sendPasswordlessLoginEmail(to: string, data: any) {
-    const template = await this.parser.render("passwordless-login.html", data);
-    await this.sendMail({ to, subject: "Login Link", html: template });
+  async getPasswordResetEmailHtml(data: SwiftPasswordResetEmailData) {
+    return this.parser.render("password-reset.html", data);
+  }
+
+  async sendPasswordlessLoginEmail(
+    {
+      from = this.config.from as string,
+      subject = "Login request",
+      ...rest
+    }: SwiftMailSendOptions,
+    data: SwiftPasswordlessLoginEmailData
+  ) {
+    const html = await this.getPasswordlessLoginEmailHtml(data);
+    return this.sendMail({ from, ...rest, html });
+  }
+
+  async getPasswordlessLoginEmailHtml(data: SwiftPasswordlessLoginEmailData) {
+    return this.parser.render("passwordless-login.html", data);
+  }
+
+  async sendWelcomeEmail(
+    {
+      from = this.config.from as string,
+      subject = "Welcome",
+      ...rest
+    }: SwiftMailSendOptions,
+    data: SwiftWelcomeEmailData
+  ) {
+    const html = await this.getWelcomeEmailHtml(data);
+    return this.sendMail({ from, ...rest, html });
+  }
+
+  async getWelcomeEmailHtml(data: SwiftWelcomeEmailData) {
+    return this.parser.render("welcome-email.html", data);
+  }
+
+  async sendAcceptInvitationEmail(
+    {
+      from = this.config.from as string,
+      subject = "You're invited to join {{companyName}}",
+      ...rest
+    }: SwiftMailSendOptions,
+    data: SwiftAcceptInvitationEmailData
+  ) {
+    const html = await this.getAcceptInvitationEmailHtml(data);
+    return this.sendMail({ from, ...rest, html });
+  }
+
+  async getAcceptInvitationEmailHtml(data: SwiftAcceptInvitationEmailData) {
+    return this.parser.render("accept-invitation.html", data);
+  }
+
+  async sendPasswordInvitationEmail(
+    {
+      from = this.config.from as string,
+      subject = "Set your password",
+      ...rest
+    }: SwiftMailSendOptions,
+    data: SwiftPasswordInvitationEmailData
+  ) {
+    const html = await this.getPasswordInvitationEmailHtml(data);
+    return this.sendMail({ from, ...rest, html });
+  }
+
+  async getPasswordInvitationEmailHtml(data: SwiftPasswordInvitationEmailData) {
+    return this.parser.render("password-invitation.html", data);
+  }
+
+  async sendWelcomeWithCredentialsEmail(
+    {
+      from = this.config.from as string,
+      subject = "Welcome to {{companyName}}",
+      ...rest
+    }: SwiftMailSendOptions,
+    data: SwiftWelcomeWithCredentialsEmailData
+  ) {
+    const html = await this.getWelcomeWithCredentialsEmailHtml(data);
+    return this.sendMail({ from, ...rest, html });
+  }
+
+  async getWelcomeWithCredentialsEmailHtml(
+    data: SwiftWelcomeWithCredentialsEmailData
+  ) {
+    return this.parser.render("welcome-with-credentials.html", data);
   }
 }
