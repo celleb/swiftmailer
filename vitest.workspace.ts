9fb5d9e75193e80ad7597b1422fcb53e831dfb22
Jonas Tomanga <celleb@mrcelleb.com>
2024-11-14 21:18:11 +0100

    feat: update dependencies for e2e tests and remove obsolete test files ✨

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,8 +1,8 @@
-import { defineWorkspace } from 'vitest/config'
+import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
   "./packages/mail/vite.config.ts",
-  "./tests/mail/vite.config.ts",
+  "./e2e/mail/vite.config.ts",
   "./packages/logger/vite.config.ts",
-  "./tests/templ/vite.config.ts"
-])
+  "./e2e/templ/vite.config.ts",
+]);
